7f24c94f5afb9a0d4f73f7000f7ef4dc3edaae21
2003arpit <97427462+2003arpit@users.noreply.github.com>
2022-01-10 14:24:34 +0530

    Add files via upload

---
+++ Asgn_1_output_21105018.py
@@ -0,0 +1,54 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:/python100/asgn_1_21105018.py ===================
+      First Question
+Enter first number:-
+23.5
+Enter second number:-
+34
+Enter third number:-
+56.89
+Average of the three numbers is:-
+ 38.13
+      Second Question
+Enter your gross income in dollars
+100000
+Enter number of dependents
+3
+Your total tax:
+ 16200.0
+        Third Question
+Enter your student id:
+21105018
+Enter your name:
+Arpit Singhal
+Enter your gender(use M for male,F for female,U for unknown):
+M
+Enter your course name:
+ECE
+Enter your CGPA:
+10.0
+[21105018, 'Arpit Singhal', 'M', 'ECE', 10.0]
+         Fourth Question
+Enter marks of first student:
+99.5
+Enter marks of second student:
+33.1
+Enter marks of third student:
+50
+Enter marks of forth student:
+45
+Enter marks of fifth student:
+88
+[33.1, 45.0, 50.0, 88.0, 99.5]
+          Part(a) Fifth Question
+Original list:
+ ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Color list after removing 4th element:
+ ['Red', 'Green', 'White', 'Pink', 'Yellow']
+       Part(b) Fifth Question
+Original list:
+ ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+List after replacing black and pink colour with purple:
+ ['Red', 'Green', 'White', 'Purple', 'Yellow']
