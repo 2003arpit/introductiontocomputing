78099427bf1e1c0b94df1c29fda3189a8d4c705e
2003arpit <97427462+2003arpit@users.noreply.github.com>
2022-02-27 23:22:49 +0530

    Add files via upload

---
+++ Assignment-4_21105018_output.py
@@ -0,0 +1,148 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================================== RESTART: C:/python100/Assignment-4_21105018.py ====================================================
+
+                         QUESTION - 1
+
+Enter number of disk in tower of Hanoi:
+4
+
+Move disk 1 from  Source Tower to Auxiliary 
+
+
+Move disk 2 from Source Tower to Destination 
+
+
+Move disk 1 from  Auxiliary to Destination 
+
+
+Move disk 3 from Source Tower to Auxiliary 
+
+
+Move disk 1 from  Destination to Source Tower 
+
+
+Move disk 2 from Destination to Auxiliary 
+
+
+Move disk 1 from  Source Tower to Auxiliary 
+
+
+Move disk 4 from Source Tower to Destination 
+
+
+Move disk 1 from  Auxiliary to Destination 
+
+
+Move disk 2 from Auxiliary to Source Tower 
+
+
+Move disk 1 from  Destination to Source Tower 
+
+
+Move disk 3 from Auxiliary to Destination 
+
+
+Move disk 1 from  Source Tower to Auxiliary 
+
+
+Move disk 2 from Source Tower to Destination 
+
+
+Move disk 1 from  Auxiliary to Destination 
+
+
+                        QUESTION - 2
+                  using RECURSIVE approach
+Enter the number of rows : 
+5
+         1   
+       1   1   
+     1   2   1   
+   1   3   3   1   
+ 1   4   6   4   1   
+
+                        QUESTION - 2
+                  using ITERATIVE approach
+Enter the number of rows:
+5
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+
+                         QUESTION - 3
+
+
+Enter an Integer : 
+200
+Enter another Integer : 
+3
+
+The quotient when 200 is divided by 3 is 66.
+
+
+The remainder when 200 is divided by 3 is 2.
+
+
+                         Que3(a)
+
+Function is Not-callable
+
+                         Que3(B)
+
+All result values are 'non-zero'
+
+                         Que3(C)
+
+Appended (4,5,6) in the values (66,2)
+Values greater than 4 is 66,5,6
+
+                         Que3(D)
+Above result in set form is shown below:
+
+{66, 5, 6}
+
+                         Que3(E)
+The above set has been converted to immutable.
+
+                         Que3(F)
+Max value from set is 66
+Hash value of 66 is 66
+
+                         QUESTION - 4
+
+Hello, My name is Arpit and my sid is 21105018
+Object Destroyed
+
+                         QUESTION - 5
+
+The present information is:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+
+                         Part(a)
+
+ Salary of Mehak after update is 70000
+
+                         Part(b)
+
+Employee Viren's data has been removed.
+
+                         QUESTION - 6
+
+
+                 WELCOME TO THE FRIENDSHIP GAME
+
+
+Enter Word by Barbie : 
+racecar
+
+Enter Word by George : 
+carrace
+
+Barbie and George are real friends!
